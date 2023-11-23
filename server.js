246dc6d55b144aec5f1bec0bd8d366ea455e3044
Maziar Pedrami <awildroot@gmail.com>
2023-11-23 14:59:56 +0100

    created DB.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+require("dotenv").config();
+
+const pool = require('./Database/ElephantSQL')
 
 app.listen(PORT, ()=>{
   console.log(`server.js running on Port ${PORT}`);
