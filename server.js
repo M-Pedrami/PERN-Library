ae41eca8d02c5a56111f931c01d7bb537cbbdd26
Maziar Pedrami <awildroot@gmail.com>
2023-11-23 22:47:34 +0100

    post function

--- server.js
+++ server.js
@@ -4,14 +4,30 @@ const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors()); // This middleware is needed to authorize the connection between Front and Back -end
+app.use(express.json()); // This middleware is needed to parse JSON in the request body
 
 const pool = require("./Database/ElephantSQL");
+
 app.get("/", (req, res) => {
   pool
     .query("SELECT * FROM users;")
     .then((data) => res.json(data))
     .catch((err) => res.status(500).json(err));
+});
+
+app.post("/", async (req, res) => {
+  const { first_name, last_name, age } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (first_name, last_name, age) VALUES ($1, $2, $3) RETURNING *",
+      [first_name, last_name, age]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => {
